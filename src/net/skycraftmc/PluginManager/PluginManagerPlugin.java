b2205fd6524c60707b98ae3ea0812b4dc588d232
Technius <techniux@gmail.com>
2013-09-01 08:51:20 -0700

    Refactoring and stuff

--- src/net/skycraftmc/PluginManager/PluginManagerPlugin.java
+++ src/net/skycraftmc/PluginManager/PluginManagerPlugin.java
@@ -2,48 +2,66 @@ package net.skycraftmc.PluginManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PluginManagerPlugin extends JavaPlugin {
+public class PluginManagerPlugin extends JavaPlugin
+{
 
 	private PluginControl control;
 	private boolean unl = false;
 
-	public PluginControl getPluginControl() {
+	public PluginControl getPluginControl()
+	{
 		return control;
 	}
 
 	@Override
-	public void onDisable() {
+	public void onDisable()
+	{
 		if (!unl)
 			control.cleanup();
 	}
 
 	@Override
-	public void onEnable() {
-		try {
+	public void onEnable()
+	{
+		try
+		{
 			control = new PluginControl();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			getLogger().severe("Failed to start!");
 			setEnabled(false);
 			e.printStackTrace();
 			return;
 		}
 
-		getCommand("pluginmanager").setExecutor(new PMCommandExecutor(this, control));
-		getServer().getScheduler().runTaskAsynchronously(this, new Runnable() {
+		getCommand("pluginmanager").setExecutor(
+				new PMCommandExecutor(this, control));
+		getServer().getScheduler().runTaskAsynchronously(this, new Runnable()
+		{
 
-			public void run() {
-				try {
-					UpdateInformation inf = Updater.findUpdate("pm-pluginmanager");
+			public void run()
+			{
+				try
+				{
+					UpdateInformation inf = Updater
+							.findUpdate("pm-pluginmanager");
 					if (!getDescription().getVersion().equals(inf.getVersion()))
-						getLogger().info("A new version of PluginManager is available: " + inf.getVersion());
-				} catch (Exception e) {
-					getLogger().warning("Could not find update: " + e.getMessage());
+						getLogger().info(
+								"A new version of PluginManager is available: "
+										+ inf.getVersion());
+				}
+				catch (Exception e)
+				{
+					getLogger().warning(
+							"Could not find update: " + e.getMessage());
 				}
 			}
 		});
 	}
 
-	void setUnload(boolean unl) {
+	void setUnload(boolean unl)
+	{
 		this.unl = unl;
 	}
 }
